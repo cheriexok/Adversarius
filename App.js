8c88fa15dc7929ac7932cef233a15ce5e18fe320
PABLO DURAN DA MAIA <pablo.maia@alunos.sc.senac.br>
2022-04-20 16:36:48 -0300

    Adicionei a navegação do StackNavigation

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Image, ImageBackground, StyleSheet} from 'react-native';
+import { Image, ImageBackground, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 
@@ -14,33 +14,52 @@ import Cad from './telas/2cad';
 import Atletacadfinal from './telas/4atletaCadFinal';
 
 import ActionBarImage from './telas/ActionBarImage';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 const Drawer = createDrawerNavigator();
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Login" screenOptions={{headerRight: () => <ActionBarImage />}} useLegacyImplementation={true}>
-        <Drawer.Screen name="Select" component={SelectS}/>
-        <Drawer.Screen name="CriaPCS" component={CriaPCS} />
-        <Drawer.Screen name="Login" component={Login} />
-        <Drawer.Screen name="Atleta" component={AtletaCad} />
-        <Drawer.Screen name="Organização" component={OrgCad} />
-        <Drawer.Screen name="Peneira" component={Exibepeneira} />
-        <Drawer.Screen name="Perfil" component={Perfil} />
-        <Drawer.Screen name='Cadastro' component={Cad} />
-
-
-      </Drawer.Navigator>
-
+      <RootNavigation />
     </NavigationContainer>
   );
 };
+export function RootNavigation() {
+  return (
+    <Stack.Navigator initialRouteName="Login" screenOptions={{ headerRight: () => <ActionBarImage /> }} useLegacyImplementation={true}>
+      <Stack.Screen name="Login" component={Login} /*options={{drawerHideStatusBarTrue}}*/ />
+      <Stack.Screen name='Cadastro' component={Cad} />
+      <Stack.Screen
+        name='TelaInicial'
+        component={InitialRoute}
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Stack.Navigator>
+  );
+};
+export function InitialRoute() {
+  return (
+    <Drawer.Navigator initialRouteName="Login" screenOptions={{ headerRight: () => <ActionBarImage /> }} useLegacyImplementation={true}>
+      <Drawer.Screen name="Select" component={SelectS} />
+      <Drawer.Screen name="CriaPCS" component={CriaPCS} />
+      <Drawer.Screen name="Login" component={Login} /*options={{drawerHideStatusBarTrue}}*/ />
+      <Drawer.Screen name="Atleta" component={AtletaCad} />
+      <Drawer.Screen name="Organização" component={OrgCad} />
+      <Drawer.Screen name="Peneira" component={Exibepeneira} />
+      <Drawer.Screen name="Perfil" component={Perfil} />
+      <Drawer.Screen name='Cadastro' component={Cad} />
+    </Drawer.Navigator>
+  );
+};
 
- const styles = StyleSheet.create({
-    image: {
-      height: 100,
-      width: 100,
-      position: 'absolute'
-    }
-  })
+const styles = StyleSheet.create({
+  image: {
+    height: 100,
+    width: 100,
+    position: 'absolute'
+  }
+})
--- telas/LoginS.js
+++ telas/LoginS.js
@@ -1,22 +1,21 @@
-import React,{useState,useEffect} from 'react';
-import {Image, Button, ImageBackground, ScrollView, Keyboard, Text, TextInput, TouchableOpacity, TouchableWithoutFeedback, View, StyleSheet} from "react-native";
+import React, { useState, useEffect } from 'react';
+import { Image, Button, ImageBackground, ScrollView, Keyboard, Text, TextInput, TouchableOpacity, TouchableWithoutFeedback, View, StyleSheet } from "react-native";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import WhiteButton from "../assets/functions/WhiteButton";
 
-export default function Login({navigation}) {
+export default function Login({ navigation }) {
 
-    const [user,setUser]=useState(null);
-    const [password,setPassword]=useState(null);
-    const [message,setMessage]=useState(null);
+    const [user, setUser] = useState(null);
+    const [password, setPassword] = useState(null);
+    const [message, setMessage] = useState(null);
 
     //Fazer Login
-    async function doLogin()
-    {
-        let reqs = await fetch(config.urlRootPhp+'Controller.php',{
+    async function doLogin() {
+        let reqs = await fetch(config.urlRootPhp + 'Controller.php', {
             method: 'POST',
-            headers:{
-                'Accept':'application/json',
-                'Content-Type':'application/json'
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
             },
             body: JSON.stringify({
                 nameUser: user,
@@ -25,61 +24,62 @@ export default function Login({navigation}) {
         });
         let ress = await reqs.json();
         Keyboard.dismiss();
-       if(ress){
-           navigation.navigate('Home');
-       }else{
+        if (ress) {
+            navigation.navigate('Home');
+        } else {
             setMessage('Usuário ou senha inválidos');
-            setTimeout(()=>{
+            setTimeout(() => {
                 setMessage(null);
-            },3000);
-       }
+            }, 3000);
+        }
     }
 
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <ImageBackground source={require('../assets/imgs/background.png')} resizeMode="cover" style={styles.imageB} >              
-                {message &&(
+            <ImageBackground source={require('../assets/imgs/background.png')} resizeMode="cover" style={styles.imageB} >
+                {message && (
                     <Text style={styles.Texto} >{message}</Text>
                 )}
                 <View style={styles.kk}>
                     <Image source={require('../assets/imgs/binoculo.png')} style={styles.image} resizeMode='stretch' ></Image>
                 </View>
                 <Text style={styles.Texto}>Login</Text>
-                
+
                 <TextInput
                     style={styles.TextI}
                     placeholder="USUÁRIO"
                     placeholderTextColor="#ccc"
-                    onChangeText={(text)=>setUser(text)}
+                    onChangeText={(text) => setUser(text)}
                 />
-                <Separator/>
+                <Separator />
                 <TextInput
                     style={styles.TextI}
                     placeholder="SENHA"
                     placeholderTextColor="#ccc"
                     secureTextEntry={true}
-                    onChangeText={(text)=>setPassword(text)}
+                    onChangeText={(text) => setPassword(text)}
                 />
-                <Separator/>    
-                <Separator/>    
-                <Separator/>    
-                <WhiteButton text="Entrar" onPress={() => navigation.navigate('')}/>
-                <Separator/>
-                <WhiteButton text="Cadastre-se" onPress={() => navigation.navigate('Cadastro')}/> 
-            </ImageBackground>       
+                <Separator />
+                <Separator />
+                <Separator />
+                <WhiteButton text="Entrar" onPress={() => navigation.navigate('')} />
+                <Separator />
+                <WhiteButton text="Cadastre-se" onPress={() => navigation.navigate('Cadastro')} />
+                {/*<TouchableOpacity onPress={() => (navigation.navigate('TelaInicial'))}><Text>Fingir Login</Text></TouchableOpacity>*/}
+            </ImageBackground>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    kk:{
-        justifyContent:'center',
-        alignItems:'center',
-    },  
+    kk: {
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
 
     image: {
-        justifyContent:'center',
-        alignItems:'center',
+        justifyContent: 'center',
+        alignItems: 'center',
         //marginBottom:100,
         width: 150,
         height: 150,
@@ -91,33 +91,33 @@ const styles = StyleSheet.create({
         width: '100%',
         height: '100%',
     },
-    
+
     separator: {
-      marginVertical: 10,
+        marginVertical: 10,
 
     },
 
     Texto: {
-      marginBottom: '10% ',
-      fontSize: 35,
-      fontWeight: 'bold',
-      marginLeft: '5%'
+        marginBottom: '10% ',
+        fontSize: 35,
+        fontWeight: 'bold',
+        marginLeft: '5%'
     },
 
     TextI: {
-      borderRadius: 25,
-      paddingVertical: 12,
-      paddingHorizontal: 12,
-      backgroundColor: 'white',
-      color: 'black',
-      fontWeight: 'bold',
-      fontSize: 20,
-      textAlign: 'center',
-      marginHorizontal: '10%',
+        borderRadius: 25,
+        paddingVertical: 12,
+        paddingHorizontal: 12,
+        backgroundColor: 'white',
+        color: 'black',
+        fontWeight: 'bold',
+        fontSize: 20,
+        textAlign: 'center',
+        marginHorizontal: '10%',
     }
 
 });
 
 const Separator = () => (
     <View style={styles.separator} />
-  );
+);
